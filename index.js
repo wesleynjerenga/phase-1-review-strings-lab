7261b0708c51ba089d5cae8e0d5ce3449740d8bf
wesleynjerenga <wesleynjerenga9@gmail.com>
2025-03-04 11:07:27 +0300

    string lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+let currentUser='Wesley'
+let welcomeMessage='Welcome to Flatbook, '
+welcomeMessage += currentUser + '!';
+let excitedWelcomeMessage=" WELCOME TO FLATBOOK, "
+excitedWelcomeMessage += currentUser.toUpperCase() + '!';
+let shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
+
